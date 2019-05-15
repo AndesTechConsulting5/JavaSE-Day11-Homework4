02ec1d105bcec570609c63d8904ab00f6a2ea5c1
iboidachenko <Ilya.BOYDACHENKO@raiffeisen.ru>
2019-05-15 14:02:35 +0300

    H/W4 - v.2

--- src/test/java/org/andestech/learning/rfb19/g4/CreditAccTest.java
+++ src/test/java/org/andestech/learning/rfb19/g4/CreditAccTest.java
@@ -1,52 +1,73 @@
 package org.andestech.learning.rfb19.g4;
 
+import org.andestech.learning.rfb19.g4.homework3.*;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
-import static org.junit.Assert.assertTrue;
 
 public class CreditAccTest {
 
-    @Test
-    @Category(IPositiveTest.class)
-    public void createAccountPositiveTest()
-    {
+    private static Customer client;
 
-    }
-
-    @Test
-    @Category(INegativeTest.class)
-    public void createAccountNegativeTest()
-    {
-
+    @BeforeClass
+    public static void init() {
+        //задан клиент
+        client = new Customer("Иван", "Петров", "ул.Ленина, д.1", 20);
     }
 
     @Test
     @Category(IPositiveTest.class)
-    public void putPositiveTest()
-    {
-
+    public void createAccountPositiveTest() throws CreditAccountException, CustomerException {
+        //открываем кредитный счет с суммой 90 клиенту
+        CreditAccount.createAccount(2, 90, client);
     }
 
-    @Test
+    @Test(expected = CreditAccountException.class)
     @Category(INegativeTest.class)
-    public void putNegativeTest()
-    {
-
+    public void createAccountNegativeTest() throws CreditAccountException, CustomerException {
+        //открываем кредитный счет с суммой -90 клиенту
+        CreditAccount.createAccount(2, -90, client);
     }
 
     @Test
     @Category(IPositiveTest.class)
-    public void withdrawalPositiveTest()
-    {
+    public void putPositiveTest() throws CreditAccountException, CustomerException, AccountException {
+        //открываем кредитный счет с суммой 1000 клиенту
+        CreditAccount.createAccount(2, 1000, client);
+        //пополняем кредитный счет у клиента
+        CreditAccount createAccount = CreditAccount.create();
+        createAccount.putMoney(100);
+    }
 
+    @Test(expected = AccountException.class)
+    @Category(INegativeTest.class)
+    public void putNegativeTest() throws AccountException, CreditAccountException, CustomerException {
+        //открываем кредитный счет с суммой 1000 клиенту
+        CreditAccount.createAccount(2, 1000, client);
+        //пополняем кредитный счет у клиента
+        CreditAccount createAccount = CreditAccount.create();
+        createAccount.putMoney(10);
     }
 
     @Test
-    @Category(INegativeTest.class)
-    public void withdrawalNegativeTest()
-    {
+    @Category(IPositiveTest.class)
+    public void withdrawalPositiveTest() throws CreditAccountException, CustomerException, AccountException {
+        //открываем кредитный счет с суммой 1000 клиенту
+        CreditAccount.createAccount(2, 1000, client);
+        //снимаем деньги с кредитного счета у клиента
+        CreditAccount createAccount = CreditAccount.create();
+        createAccount.withdrawal(100);
+    }
 
+    @Test(expected = AccountException.class)
+    @Category(INegativeTest.class)
+    public void withdrawalNegativeTest() throws CreditAccountException, CustomerException, AccountException {
+        //открываем кредитный счет с суммой 1000 клиенту
+        CreditAccount.createAccount(2, 1000, client);
+        //снимаем деньги с кредитного счета у клиента
+        CreditAccount createAccount = CreditAccount.create();
+        createAccount.withdrawal(10);
     }
 
 }
--- src/test/java/org/andestech/learning/rfb19/g4/DebitAccTest.java
+++ src/test/java/org/andestech/learning/rfb19/g4/DebitAccTest.java
@@ -1,53 +1,84 @@
 package org.andestech.learning.rfb19.g4;
 
-import static org.junit.Assert.assertTrue;
-
+import org.andestech.learning.rfb19.g4.homework3.AccountException;
+import org.andestech.learning.rfb19.g4.homework3.Customer;
+import org.andestech.learning.rfb19.g4.homework3.CustomerException;
+import org.andestech.learning.rfb19.g4.homework3.DebitAccount;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
 
 public class DebitAccTest {
 
-    @Test
-    @Category(IPositiveTest.class)
-    public void createAccountPositiveTest()
-    {
 
-    }
+    private static Customer client;
+    private static DebitAccount debit;
 
-    @Test
-    @Category(INegativeTest.class)
-    public void createAccountNegativeTest()
-    {
-
+    @BeforeClass
+    public static void init() {
+        debit = new DebitAccount();
     }
 
     @Test
     @Category(IPositiveTest.class)
-    public void putPositiveTest()
-    {
+    public void createAccountPositiveTest() throws CustomerException {
+        //задан клиент
+        client = new Customer("Иван", "Петров", "ул.Ленина, д.1", 20);
+        //открываем дебетовый счет с суммой 100 клиенту
+        debit.debitAccount(1, 100, client);
+    }
 
+    @Test(expected = CustomerException.class)
+    @Category(INegativeTest.class)
+    public void createAccountNegativeTest() throws CustomerException {
+        //задан клиент
+        client = new Customer("Иван", "Петров", "ул.Ленина, д.1", 17);
+        //открываем дебетовый счет с суммой 100 клиенту
+        debit.debitAccount(1, 100, client);
     }
 
     @Test
+    @Category(IPositiveTest.class)
+    public void putPositiveTest() throws AccountException {
+        //задан клиент
+        client = new Customer("Иван", "Петров", "ул.Ленина, д.1", 18);
+        //пополняем дебетовый счет клиенту
+        debit.putMoney(500);
+
+    }
+
+    @Test(expected = AccountException.class)
     @Category(INegativeTest.class)
-    public void putNegativeTest()
-    {
+    public void putNegativeTest() throws AccountException {
+        //задан клиент
+        client = new Customer("Иван", "Петров", "ул.Ленина, д.1", 18);
+        //пополняем дебетовый счет клиенту
+        debit.putMoney(50);
 
     }
 
     @Test
     @Category(IPositiveTest.class)
-    public void withdrawalPositiveTest()
-    {
+    public void withdrawalPositiveTest() throws AccountException {
+        //задан клиент
+        client = new Customer("Иван", "Петров", "ул.Ленина, д.1", 18);
+        //пополняем дебетовый счет клиенту
+        debit.putMoney(500);
+        //снимает деньги с дебетового счета у клиента
+        debit.withdrawal(100);
 
     }
 
-    @Test
+    @Test(expected = AccountException.class)
     @Category(INegativeTest.class)
-    public void withdrawalNegativeTest()
-    {
-
+    public void withdrawalNegativeTest() throws AccountException {
+        //задан клиент
+        client = new Customer("Иван", "Петров", "ул.Ленина, д.1", 18);
+        //пополняем дебетовый счет клиенту
+        debit.putMoney(500);
+        //снимает деньги с дебетового счета у клиента
+        debit.withdrawal(50);
     }
 
 }
