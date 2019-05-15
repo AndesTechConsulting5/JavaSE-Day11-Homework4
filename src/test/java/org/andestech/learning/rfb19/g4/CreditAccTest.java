a62686509e188e5fd9837c9400e2e96cd9ae4aed
iboidachenko <Ilya.BOYDACHENKO@raiffeisen.ru>
2019-05-15 12:27:24 +0300

    H/W4 - v.1

---
+++ src/test/java/org/andestech/learning/rfb19/g4/CreditAccTest.java
@@ -0,0 +1,52 @@
+package org.andestech.learning.rfb19.g4;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.assertTrue;
+
+public class CreditAccTest {
+
+    @Test
+    @Category(IPositiveTest.class)
+    public void createAccountPositiveTest()
+    {
+
+    }
+
+    @Test
+    @Category(INegativeTest.class)
+    public void createAccountNegativeTest()
+    {
+
+    }
+
+    @Test
+    @Category(IPositiveTest.class)
+    public void putPositiveTest()
+    {
+
+    }
+
+    @Test
+    @Category(INegativeTest.class)
+    public void putNegativeTest()
+    {
+
+    }
+
+    @Test
+    @Category(IPositiveTest.class)
+    public void withdrawalPositiveTest()
+    {
+
+    }
+
+    @Test
+    @Category(INegativeTest.class)
+    public void withdrawalNegativeTest()
+    {
+
+    }
+
+}
---
+++ src/test/java/org/andestech/learning/rfb19/g4/DebitAccTest.java
@@ -0,0 +1,53 @@
+package org.andestech.learning.rfb19.g4;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+
+public class DebitAccTest {
+
+    @Test
+    @Category(IPositiveTest.class)
+    public void createAccountPositiveTest()
+    {
+
+    }
+
+    @Test
+    @Category(INegativeTest.class)
+    public void createAccountNegativeTest()
+    {
+
+    }
+
+    @Test
+    @Category(IPositiveTest.class)
+    public void putPositiveTest()
+    {
+
+    }
+
+    @Test
+    @Category(INegativeTest.class)
+    public void putNegativeTest()
+    {
+
+    }
+
+    @Test
+    @Category(IPositiveTest.class)
+    public void withdrawalPositiveTest()
+    {
+
+    }
+
+    @Test
+    @Category(INegativeTest.class)
+    public void withdrawalNegativeTest()
+    {
+
+    }
+
+}
